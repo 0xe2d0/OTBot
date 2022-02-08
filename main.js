2fd8bc1699f06da81310577a7984d503763803da
Eyyüb <89468647+0xe2d0@users.noreply.github.com>
2022-02-08 19:10:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { Client, Intents } = require("discord.js");
+const client = new Client({intents : [Intents.FLAGS.GUILDS] })
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+const data = fs.readFileSync("config.json");
+const obj = JSON.parse(data);
+
+ 
+  
+client.login(obj["bot-settings"]["token"]);
+ 
+app.get("/", (req,res) => {
+	res.sendFile(__dirname+"/index.html")
+});
+
+app.post("/", async (req, res) => {
+	if(req.headers["get-link"] == "true"){
+		client.guilds.cache.map(async guild => {
+			client.user.setActivity("Generate A Link",{ type : "PLAYING" });
+		
+			const channel = guild.channels.cache.filter(cnl => cnl.type == "GUILD_TEXT").first();
+		
+			await channel.createInvite({
+			
+				maxAge: obj["bot-settings"]["max-age"],
+			
+				maxUses:obj["bot-settings"]["max-uses"]
+		
+			}).then(async (invite) => {
+				client.user.setActivity();
+				res.send(`${invite.url}`);
+			})
+		});
+	}
+	else{
+		res.send("Header Argument Not Found!")
+	}
+
+});
+
+app.listen(obj["site-settings"]["port"],() => {
+console.log("Server running in http://0.0.0.0:"+obj["site-settings"]["port"])
+});
